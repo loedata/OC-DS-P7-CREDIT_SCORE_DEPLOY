b847d40212470481a7d8b909c4c1aba795393240
loedata <loe.rabier@gmail.com>
2021-08-09 15:31:59 +0200

    OC-DS-P7-ML Interprétabilité du modèle avec les features importances du modèle LightGBM et calculs/graphiques des SHAPLEY values.

--- OC-DS-P7-MODELLING/notebooks/outils_model.py
+++ OC-DS-P7-MODELLING/notebooks/outils_model.py
@@ -1658,6 +1658,64 @@ def plot_permutation_importance(model, x_test, y_test):
              perm_importance.importances_mean[sorted_idx])
     plt.xlabel("Permutation Importance (%)")
     plt.show()
+    
+def plot_cumultative_feature_importance(df, threshold = 0.9):
+    """
+    Plots 15 most important features and the cumulative importance of features.
+    Prints the number of features needed to reach threshold cumulative importance.
+    
+    Parameters
+    --------
+    df : dataframe
+        Dataframe of feature importances. Columns must be feature and importance
+    threshold : float, default = 0.9
+        Threshold for prining information about cumulative importances
+        
+    Return
+    --------
+    df : dataframe
+        Dataframe ordered by feature importances with a normalized column (sums to 1)
+        and a cumulative importance column
+    
+    """
+    
+    plt.rcParams['font.size'] = 18
+    
+    # Sort features according to importance
+    df = df.sort_values('importance', ascending = False).reset_index()
+    
+    # Normalize the feature importances to add up to one
+    df['importance_normalized'] = df['importance'] / df['importance'].sum()
+    df['cumulative_importance'] = np.cumsum(df['importance_normalized'])
+
+    # Make a horizontal bar chart of feature importances
+    plt.figure(figsize = (10, 6))
+    ax = plt.subplot()
+    
+    # Need to reverse the index to plot most important on top
+    ax.barh(list(reversed(list(df.index[:15]))), 
+            df['importance_normalized'].head(15), 
+            align = 'center', edgecolor = 'k')
+    
+    # Set the yticks and labels
+    ax.set_yticks(list(reversed(list(df.index[:15]))))
+    ax.set_yticklabels(df['feature'].head(15))
+    
+    # Plot labeling
+    plt.xlabel('Normalized Importance'); plt.title('Feature Importances')
+    plt.show()
+    
+    # Cumulative importance plot
+    plt.figure(figsize = (8, 6))
+    plt.plot(list(range(len(df))), df['cumulative_importance'], 'r-')
+    plt.xlabel('Number of Features'); plt.ylabel('Cumulative Importance'); 
+    plt.title('Cumulative Feature Importance');
+    plt.show();
+    
+    importance_index = np.min(np.where(df['cumulative_importance'] > threshold))
+    print('%d variables nécessaires pour %0.2f de cummulative imortance' % (importance_index + 1, threshold))
+    
+    return df
 
 # -----------------------------------------------------------------------
 # -- PLOT LES SHAP VALUES AVEC ELI5
@@ -2548,6 +2606,6 @@ def afficher_matrice_confusion(y_true, y_pred, title):
     plt.xlabel('Classe prédite')
     plt.show()    
 
-# -----------------------------------------------------------------------
-# -- OPTIMISATION HYPERPARAMETRE 
-# -----------------------------------------------------------------------
+
+
+
